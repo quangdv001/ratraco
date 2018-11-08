728d0e62054915c8ef00b97de893053cf7958c17
Quang Do Vinh <quangdovinh@vccorp.vn>
2018-11-08 10:51:45 +0700

    permission

--- app/Http/Controllers/Admin/AdminPermissionController.php
+++ app/Http/Controllers/Admin/AdminPermissionController.php
@@ -2,24 +2,29 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Http\Requests\Admin\AccountRequest;
 use App\Services\AdminService;
+use App\Services\PermissionService;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class AdminPermissionController extends AdminBaseController
 {
     protected $admin;
-    public function __construct(AdminService $adminService)
+    protected $permission;
+    public function __construct(AdminService $adminService, PermissionService $permissionService)
     {
         parent::__construct();
         $this->admin = $adminService;
+        $this->permission = $permissionService;
     }
 
     public function index(Request $request){
-//        $params = $request->only('username', 'email', 'phone', 'name', 'active');
-//        $data = $this->admin->search($params);
-        $data = $this->admin->getAll();
-        return view('admin.account.index', ['data', $data]);
+        $params = $request->only('username', 'email', 'phone', 'name', 'active');
+        $data = $this->admin->search($params);
+        debug($data);
+        return view('admin.permission.index')
+            ->with('data', $data);
     }
 
     public function getCreate($id = 0){
@@ -27,10 +32,36 @@ class AdminPermissionController extends AdminBaseController
         if($id > 0){
             $data = $this->admin->getById($id);
         }
-        return view('admin.account.edit', ['data', $data]);
+        $group = $this->permission->getAll();
+        debug($group, $data);
+        return view('admin.account.edit')
+            ->with('id', $id)
+            ->with('group', $group)
+            ->with('data', $data);
     }
 
-    public function postCreate($id = 0, Request $request){
+    public function postCreate(AccountRequest $request, $id = 0){
+        $data = $request->only('username', 'email', 'phone', 'name', 'active');
+        $mess = '';
+        if($request->filled('password')){
+            $data['password'] = bcrypt($request->filled('password'));
+        }
+        if($request->has('permission')){
+            $data['permissions'] = implode(',',$request->input('permission'));
+        }
+        if($id == 0){
+            $res = $this->admin->create($data);
+            if($res){
+                $mess = 'Tạo tài khoản thành công';
+            }
+        } else {
+            $admin = $this->admin->getById($id);
+            $res = $this->admin->update($admin, $data);
+            if($res){
+                $mess = 'Cập nhật tài khoản thành công';
+            }
+        }
+        return redirect()->route('admin.account.getList')->with('success_message', $mess);
     }
 
 }
--- public/assets/admin/js/init.js
+++ public/assets/admin/js/init.js
@@ -16,12 +16,10 @@ $(document).ready(function () {
     // });
 
     //preloader
-    setTimeout(function(){
-        $('#loader-wrapper, #loader').hide();
-        $('.container-scroller').show();
+    setTimeout(function () {
+        init.hideLoader();
     }, 1000);
-
-    $('.forms-sample').submit(function(){
+    $('.forms-sample').submit(function () {
         $('.has-spinner').buttonLoader('start');
     });
 
@@ -31,24 +29,24 @@ $(document).ready(function () {
     // show alert after redirect
     var success_message = $('.success_message').val();
     var error_message = $('.error_message').val();
-    if(success_message){
-        global.notyPopup(success_message,'success',2500);
+    if (success_message) {
+        global.notyPopup(success_message, 'success', 2500);
     }
-    if(error_message){
-        init.notyPopup(error_message,'error',2500);
+    if (error_message) {
+        init.notyPopup(error_message, 'error', 2500);
     }
 });
 
 var init = {
-    notyPopup: function(text,type,time){
+    notyPopup: function (text, type, time) {
         var icon = '';
-        if(type === 'success'){
+        if (type === 'success') {
             icon = '<i class="fa fas fa-check"></i>';
         }
-        if(type === 'error'){
+        if (type === 'error') {
             icon = '<i class="fa fas fa-times"></i>';
         }
-        var txt = "<span class='noty-text'>"+ icon + " " + text +"</span>";
+        var txt = "<span class='noty-text'>" + icon + " " + text + "</span>";
         new Noty({
             theme: 'bootstrap-v4',
             type: type,
@@ -56,5 +54,14 @@ var init = {
             layout: 'topRight',
             text: txt
         }).show();
+    },
+    showLoader: function () {
+        $('#loader-wrapper, #loader').show();
+        $('.container-scroller').hide();
+    },
+    hideLoader: function () {
+        $('#loader-wrapper, #loader').hide();
+        $('.container-scroller').show();
     }
+
 };
